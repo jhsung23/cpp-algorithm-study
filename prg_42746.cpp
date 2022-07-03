446e7827d003882c636d817784019c2ba5ae4f7e
sungjihyun <jhsung23@gmail.com>
2022-07-03 22:36:03 +0900

    2022.07.03

--- prg_42746.cpp
+++ prg_42746.cpp
@@ -1,24 +1,31 @@
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
+bool compare(const string &a, const string &b)
+{
+    if (b + a < a + b)
+        return true;
+    return false;
+}
+
 string solution(vector<int> numbers) {
     string answer = "";
-    
-    sort(numbers.begin(), numbers.end());
-    
-    do{
-        string tmpstring="";
-        for(int i=0;i<numbers.size();i++){
-            tmpstring+=to_string(numbers[i]);
-        }
-        if(answer<tmpstring) {
-            answer=tmpstring;
-        }
-    }while(next_permutation(numbers.begin(), numbers.end()));
-    
+
+    vector<string> strings;
+
+    for (int i : numbers)
+        strings.push_back(to_string(i));
+
+    sort(strings.begin(), strings.end(), compare);
+
+    for (auto iter = strings.begin(); iter < strings.end(); ++iter)
+        answer += *iter;
+
+    if (answer[0] == '0')
+        answer = "0";
+
     return answer;
 }
