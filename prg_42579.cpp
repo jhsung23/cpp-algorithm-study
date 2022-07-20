f6a3c168b5b0778f63a0235363725b21eca1fc7f
sungjihyun <jhsung23@gmail.com>
2022-07-21 00:35:02 +0900

    2022.07.20

--- prg_42579.cpp
+++ prg_42579.cpp
@@ -7,48 +7,50 @@
 
 using namespace std;
 
+bool compare(pair<string, int> &a, pair<string, int> &b){
+    return a.second>b.second;
+}
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
-    vector<int> tmp;
     multiset<string> ms;
-    map<int, string> m;
-    map<int, int> musicNumber;
-    
+    map<string, int> m;
+    multimap<int, string> m2;
+    map<int, int> playandnumber;
+
     for(int i=0;i<genres.size();i++){
+        if(m.find(genres[i])!=m.end()){
+            m[genres[i]]+=plays[i];
+        }else m[genres[i]]=plays[i];
         if(ms.count(genres[i])<2) ms.insert(genres[i]);
-        m.insert(make_pair(plays[i], genres[i]));
-        musicNumber.insert(make_pair(plays[i], i));
+        m2.insert(pair<int, string>(plays[i], genres[i]));
+        playandnumber.insert(make_pair(i, plays[i]));
     }
     
-    while(!ms.empty()){
-        string curGenre=m.rbegin()->second;
-        int curPlay=m.rbegin()->first;
-        
-        //1번
-        if(ms.count(curGenre)<0) continue;
-        
-        tmp.push_back(curPlay);
-        m.erase(m.find(curPlay));
-        ms.erase(ms.find(curGenre));
-        
-        //2번
-        if(ms.count(curGenre)<0) continue;
-        
-        for(auto it=m.rbegin();it!=m.rend();it++){
-            if(it->second==curGenre){
-                tmp.push_back(it->first);
-                m.erase(m.find(it->first));
-                ms.erase(ms.find(curGenre));
+    vector<pair<string, int> > v;
+    for(auto it=ms.begin();it!=ms.end();it++){
+        v.push_back(make_pair(*it, m[*it]));
+    }
+    sort(v.begin(), v.end(), compare);
+
+    vector<int> tmp;
+    for(int i=0;i<v.size();i++){
+            for(auto it=m2.rbegin();it!=m2.rend();it++){
+                if(it->second==v[i].first){
+                    tmp.push_back(it->first);
+                    m2.erase(m2.find(it->first));
+                    break;
+                }
+            }
+    }
+    
+    for(int i=0;i<tmp.size();i++){
+        for(auto it=playandnumber.begin();it!=playandnumber.end();it++){
+            if(it->second==tmp[i]){
+                answer.push_back(it->first);
+                playandnumber.erase(it);
+                
             }
         }
-    }
-    
-    for(int i=0;i<tmp.size();i++){
-        answer.push_back(musicNumber[tmp[i]]);
-    }
-    
-    for(int i=0;i<tmp.size();i++){
-        cout<<tmp[i]<<" ";
     }
     return answer;
 }
