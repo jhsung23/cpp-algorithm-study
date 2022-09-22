f4923950ff18c91f1e85ad2c2860b071a94fbbc8
sungjihyun <jhsung23@gmail.com>
2022-09-22 21:43:46 +0900

    2022.09.22

--- prg_ 72412.cpp
+++ prg_ 72412.cpp
@@ -2,39 +2,60 @@
 #include <vector>
 #include <algorithm>
 #include <sstream>
+#include <map>
 using namespace std;
 
 vector<int> solution(vector<string> info, vector<string> query) {
-    vector<pair<int, string> > infoString;
-    vector<int> answer(query.size(), 0);
+    map<string, vector<int> > infoMap;
+    vector<int> answer;
     
     for(int i=0;i<info.size();i++){
         string language, job, career, soulfood, score, tmpstring="";
         stringstream iString(info[i]);
         iString >> language >> job >> career >> soulfood >> score;
-        tmpstring+=language[0];
-        tmpstring+=job[0];
-        tmpstring+=career[0];
-        tmpstring+=soulfood[0];
         
-        infoString.push_back(make_pair(stoi(score), tmpstring));
+        vector<vector<string> > charVec(4, vector<string> (2, "-"));
+        
+        charVec[0][0]=language[0];
+        charVec[1][0]=job[0];
+        charVec[2][0]=career[0];
+        charVec[3][0]=soulfood[0];
+        
+        for(int a=0;a<2;a++){
+            string strL, strJ, strC, strS;
+            strL=charVec[0][a];
+            for(int b=0;b<2;b++){
+                strJ=charVec[1][b];
+                for(int c=0;c<2;c++){
+                    strC=charVec[2][c];
+                    for(int d=0;d<2;d++){
+                        strS=charVec[3][d];
+                        infoMap[strL+strJ+strC+strS].push_back(stoi(score));
+                    }
+                }
+            }
+        }
+        infoMap[tmpstring].push_back(stoi(score));
     }
     
-    sort(infoString.begin(), infoString.end());
+    for(auto it=infoMap.begin();it!=infoMap.end();it++){
+        sort(it->second.begin(), it->second.end());
+    }
+    
     
     for(int i=0;i<query.size();i++){
-        string l="", a1="", j="", a2="", c="", a3="", f="", s="";
-        stringstream queryString(query[i]);
-        queryString >> l >> a1 >> j >> a2 >> c >> a3 >> f >> s;
-        for(int k=0;k<infoString.size();k++){
-            string str=infoString[k].second;
-            if(l[0]!='-' && str[0]!=l[0]) continue;
-            if(j[0]!='-' && str[1]!=j[0]) continue;
-            if(c[0]!='-' && str[2]!=c[0]) continue;
-            if(f[0]!='-' && str[3]!=f[0]) continue;
-            if(infoString[k].first>=stoi(s)) answer[i]+=1;
-            else break;
-        }
+        string l="", a1="", j="", a2="", c="", a3="", f="", s="", queryString="";
+        stringstream qString(query[i]);
+        qString >> l >> a1 >> j >> a2 >> c >> a3 >> f >> s;
+        
+        queryString+=l[0];
+        queryString+=j[0];
+        queryString+=c[0];
+        queryString+=f[0];
+        
+        int n=lower_bound(infoMap[queryString].begin(), infoMap[queryString].end(), stoi(s))-infoMap[queryString].begin();
+        answer.push_back(infoMap[queryString].size()-n);
     }
+    
     return answer;
 }
